41437c6fbe71765281552d3ee1c4564f118fdef9
Svitlana Gadion <svitlanagadion@gmail.com>
2025-04-02 23:37:14 +0300

    add js task2,3

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,10 @@
 'use strict'
 
-const sortByDescendingFriendCount = (users) =>
-  users.toSorted((a, b) => b.friends.length - a.friends.length);
-
+const sortByDescendingFriendCount = users => {
+  return users.toSorted(
+    (a, b) => b.friends.length - a.friends.length
+  );
+};
 
 console.log(
   sortByDescendingFriendCount([
